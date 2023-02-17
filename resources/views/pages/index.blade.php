6bcb49859374167187ec30148bde19f12b2c0535
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-18 01:02:04 +0800

    update landing page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,10 +6,11 @@
 
 @section('content')
     <div class="d-flex flex-column justify-content-center align-items-center vh-100 text-center text-dark">
+        <img src="{{ asset('img/feedat-logo.png') }}" class="img-logo" alt="feedat logo">
         <h1 class="font-weight-bold">Feedat!</h1>
         <p class="mb-6">Receive constructive feedback from your friends and co-workers.</p>
 
-        <a href="{{ route('login') }}" class="btn btn-primary rounded-pill min-w-300px mb-5">Login</a>
-        <a href="{{ route('register') }}" class="btn btn-primary rounded-pill min-w-300px">Register</a>
+        <a href="{{ route('login') }}" class="btn btn-default rounded-pill min-w-300px mb-5">Login</a>
+        <a href="{{ route('register') }}" class="btn btn-default rounded-pill min-w-300px">Register</a>
     </div>
 @endsection
