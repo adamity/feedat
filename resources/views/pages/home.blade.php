b57b6a0ff0fbd35255090d075c0e529906aed455
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-22 23:49:35 +0800

    update base url

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-    <p id="usrname" onclick="copyToClipboard('#usrname')" class="btn btn-default">feedat.test/{{ Auth::user()->user_id }}</p>
+    <p id="usrname" onclick="copyToClipboard('#usrname')" class="btn btn-default">feedat.zulkiflizin.com/{{ Auth::user()->user_id }}</p>
     <a href="/message" class="btn btn-default">View Messages</a>
     <a href="/user" class="btn btn-default">Settings</a>
 @endsection
