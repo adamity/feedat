d3b635ad493cbbf680322bb2fca368ba582d378e
Adamity <50904284+adamity@users.noreply.github.com>
2023-02-22 21:01:11 +0800

    add demo only badge

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,6 +12,7 @@
     <title>@yield('title'){{ config('app.name', 'Feedat') }}</title>
 
     <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
@@ -25,7 +26,9 @@
     </div>
 
     <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="{{ asset('js/modify.js') }}"></script>
 </body>
 
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,6 +9,7 @@
         <img src="{{ asset('img/feedat-logo.png') }}" class="img-logo" alt="feedat logo">
         <h1 class="font-weight-bold">Feedat!</h1>
         <p class="mb-6">Receive constructive feedback from your friends and co-workers.</p>
+        <p class="d-inline-block bg-danger-subtle border border-danger rounded-pill text-danger user-select-none small fw-semibold px-3 py-1 mb-6" data-bs-toggle="tooltip" data-bs-title="This is a demo site, all the date will be deleted without notice.">Demo Only ⓘ</p>
 
         <a href="{{ route('login') }}" class="btn btn-default rounded-pill min-w-300px mb-5">Login</a>
         <a href="{{ route('register') }}" class="btn btn-default rounded-pill min-w-300px">Register</a>
